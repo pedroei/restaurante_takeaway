6d5a9e647fb0ee99e9d1b6a0d0312d9639b3d844
Pedro <pedroaraujo_41@hotmail.com>
2020-12-02 10:49:25 +0000

    First part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import productsList from './products.json';
+
+const PRODUCTS_PAGE = 'products';
+const CART_PAGE = 'cart';
 
 function App() {
+  const [products] = useState(productsList);
+  const [cart, setCart] = useState([]);
+  const [page, setPage] = useState(PRODUCTS_PAGE);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeProduct = (product) => {};
+
+  const productsPage = () => (
+    <>
+      <h1>Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <h3>{product.name}</h3>
+            <h4>{product.price}€</h4>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => addToCart(product)}>
+              Adicionar ao carrinho
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+
+  const cartPage = () => (
+    <>
+      <h1>Carrinho de compras</h1>
+      <div className="products">
+        {cart.map((product) => (
+          <div className="product" key={product.id}>
+            <h3>{product.name}</h3>
+            <h4>{product.price}€</h4>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => removeProduct(product)}>Remover</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+
+  const changePage = (nextPage) => {
+    setPage(nextPage);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <button onClick={() => changePage(CART_PAGE)}>
+          Carrinho de compras({cart.length})
+        </button>
+        <button onClick={() => changePage(PRODUCTS_PAGE)}>Produtos</button>
       </header>
+      {page === PRODUCTS_PAGE && productsPage()}
+      {page === CART_PAGE && cartPage()}
     </div>
   );
 }
